e2d1ab72377da6018c5868e99a947fb188282382
Henk Hornschuh <henk778@gmail.com>
2024-04-10 10:30:21 +0200

    TMPR-0 added metric system for readouts

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -2,8 +2,14 @@ import axios from 'axios';
 import WeatherJsonSanitizer from '../sanitizer/jsonSanatizer';
 
 export default class Weather {
+  private readonly longitude = 'lon';
+  private readonly latitude = 'lat';
+  private readonly appId = 'appid';
+  private readonly unit = 'units';
+
   private readonly requestUrl = process.env.OPEN_WEATHER_CURRENT_API_URL || '';   
   private readonly apiKey = process.env.OPEN_WEATHER_API_KEY || '';
+  private readonly metric = process.env.METRIC || 'metric';
 
   async getWeather(lat: number, lon: number){
     const url = this.buildApiUrl();
@@ -27,9 +33,10 @@ export default class Weather {
   private buildUrlParams(lat: number, lon: number): URLSearchParams {
     const url = new URLSearchParams();
 
-    url.append('lon', lon.toString());
-    url.append('lat', lat.toString());
-    url.append('appid', this.apiKey);
+    url.append(this.longitude, lon.toString());
+    url.append(this.latitude, lat.toString());
+    url.append(this.appId, this.apiKey);
+    url.append(this.unit, this.metric);
 
     return url;
   }
